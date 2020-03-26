30eb28753e5d32f0e6d302075dda1d1e2a8dbac1
Tom <shmiffeh@gmail.com>
2020-03-26 11:51:45 +0000

    Add scene switching
    Game will now switch to the arena scene after pressing the start button

--- Box of Bolts/Scripts/Main.gd
+++ Box of Bolts/Scripts/Main.gd
@@ -1,10 +1,38 @@
 extends Node
 
 
+var currentScene = null
+var currentSceneNode = null
+
+const MainMenu = preload("res://Scenes/MainMenu.tscn")
+const Arena = preload("res://Scenes/Arena.tscn")
+
+var scenes = {"MainMenu": MainMenu, "Arena": Arena}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Player.hide()
-	$MainMenu.show()
-	pass # Replace with function body.
+	change_scene("MainMenu")
+	
 
+
+func change_scene(newScene):
+	if scenes.has(newScene):
+		if(currentScene != null):
+			unload_scene(scenes[currentScene])
+		currentScene = newScene
+		load_scene(scenes[currentScene])
+		
+		return true
+	else:
+		return false
+	pass
+
+func unload_scene(scene):
+	currentSceneNode.queue_free()
+	
+func load_scene(scene):
+	currentSceneNode = scene.instance()
+	add_child(currentSceneNode)
+
+func _on_MainMenu_start_game():
+	change_scene("Arena")
--- Box of Bolts/Scripts/MainMenu.gd
+++ Box of Bolts/Scripts/MainMenu.gd
@@ -1,5 +1,9 @@
 extends CanvasLayer
 
+signal start_game
+
+
+var player
 
 func hide():
 	$Label.hide()
@@ -8,3 +12,11 @@ func hide():
 func show():
 	$Label.show()
 	$Button.show()
+
+func _ready():
+	self.connect("start_game", self.get_parent(), "_on_MainMenu_start_game")
+	
+
+func _on_Button_start_fight():
+	emit_signal("start_game")
+	
