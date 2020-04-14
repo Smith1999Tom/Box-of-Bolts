e356acb852b0c3873047bd0ceba9267e1755b134
Tom <shmiffeh@gmail.com>
2020-04-14 10:48:44 +0100

    Make player step forward over time
    Also implemented a speed scaling factor. Players will step forward the same distance, but faster or slower

--- Box of Bolts/Scenes/Player/Player.gd
+++ Box of Bolts/Scenes/Player/Player.gd
@@ -1,17 +1,25 @@
 extends Mech
 
 export var stepForwardDistance = 1280*0.125
+export var stepForwardSpeed = 1.0
+
 var screenpos
 var state = "Idle"
+
 
 func _ready():
 	$AnimatedSprite.play("Idle")
 	write_health()
 	screenpos = get_viewport_rect().size
 	position.x = 1280*0.25
+	
 	pass
 		
 	
+func _process(delta):
+	if(state == "StepForward"):
+		self.position.x += (stepForwardDistance * delta * stepForwardSpeed)
+	pass	
 	
 func write_health():
 	print(self.health)
@@ -20,13 +28,13 @@ func rPunch():
 	$AnimatedSprite.play("RPunch")
 	
 func stepForward():
+	$AnimatedSprite.speed_scale = stepForwardSpeed*2
 	$AnimatedSprite.play("StepForward")
 	state = "StepForward"
 	#self.position.x += stepForwardDistance
 
 
 func _on_AnimatedSprite_animation_finished():
-	if(state == "StepForward"):
-		self.position.x += stepForwardDistance
-		state = "Idle"
+	state = "Idle"
+	$AnimatedSprite.speed_scale = 1
 	$AnimatedSprite.play("Idle")
